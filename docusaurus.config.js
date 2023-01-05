31572719a087b69d2ea1e8555abb1dd92424bad4
Anuar <anuar.kubenov@foropay.eu>
2023-01-05 12:55:08 +0600

    change config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Foropay docs",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://foropay.github.io",
+  baseUrl: "/docs-portal/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
